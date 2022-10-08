6bcd600620057e1b09b159e358cf8f2ff7f6d54e
Ginder <46971734+Ginder-Singh@users.noreply.github.com>
2022-10-08 14:19:57 -0400

    Initial Commit

---
+++ LockWidget/LockWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockWidgetApp.swift
+//  LockWidget
+//
+//  Created by Ginder Singh on 2022-10-08.
+//
+
+import SwiftUI
+
+@main
+struct LockWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
